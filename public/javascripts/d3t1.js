8e90431d9ee8df7b912c1c7d2e88aa1ef2d9f9d9
freecdog <cdog@ya.ru>
2014-08-11 18:07:40 +0400

    first test d3t1

--- public/javascripts/d3t1.js
+++ public/javascripts/d3t1.js
@@ -1,3 +1,10 @@
 /**
  * Created by jaric on 11.08.2014.
  */
+
+(function (d3){
+    console.log("d3t1");
+    d3.selectAll("p").data([1,3,5,7,9]).style("color", function() {
+        return "hsl(" + Math.random() * 360 + ",100%,50%)";
+    }).append("p").text(function(d){return "number"+d});
+})(d3);
--- routes/d3t1.js
+++ routes/d3t1.js
@@ -1,3 +1,12 @@
 /**
  * Created by jaric on 11.08.2014.
  */
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('d3t1', { title: 'd3t1' });
+});
+
+module.exports = router;
