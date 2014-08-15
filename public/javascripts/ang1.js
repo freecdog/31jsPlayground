2e9fc6117865c40329d3ab6408c4c0998930e254
freecdog <cdog@ya.ru>
2014-08-15 20:56:52 +0400

    angular tutorial 5,6 completed, but query fails with base64 images.

--- public/javascripts/ang1.js
+++ public/javascripts/ang1.js
@@ -9,11 +9,14 @@
     console.log("ang1", angular);
 
     var jApp = angular.module('jApp', []);
-    console.log(jApp);
+    console.log("jApp", jApp);
 
-    jApp.controller('jListCtrl', function($scope) {
+    //jApp.controller('jListCtrl', function($scope, $http) {
+    // in angular tutorial 5 described why it's  better to use such method.
+    // The main reason why to do this is code minify technique which one is changing variable names.
+    jApp.controller('jListCtrl', ['$scope', '$http', function($scope, $http) {
 
-        $scope.jElements = [
+        /*$scope.jElements = [
             {
                 'name': 'Qwe',
                 'snippet': 'qsc qscr',
@@ -29,8 +32,15 @@
                 'snippet': 'zwd zwdv',
                 'date': (new Date()).getMilliseconds() + 2500
             }
-        ];
+        ];*/
 
+        $http.get('/ang1/data').success(function(data){
+            console.log("data fetched", data);
+            console.warn("Query ng-model fails with base64 image, don't know why it used to find them, but it is");
+            $scope.jElements = data;
+        });
+
+        // to descent order use '-name'
         $scope.jOrderProp = 'name';
-    });
+    }]);
 })(angular);
